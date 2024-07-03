4ece6dac098b282139e9fca852005aa744963055
roshan-jha-23 <jha062305@gmail.com>
2024-07-04 02:28:25 +0530

    authentication and navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
+import Reviews from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -41,37 +44,167 @@ export default function Home() {
               </ul>
               <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
                 <div className="flex -space-x-4">
-                  <img src="/users/user-1.png" alt="user image" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
-                  <img src="/users/user-2.png" alt="user image" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
-                  <img src="/users/user-3.png" alt="user image" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
-                  <img src="/users/user-4.jpg" alt="user image" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
-                  <img src="/users/user-5.jpg" alt="user image" className="inline-block h-10 w-10 object-cover rounded-full ring-2 ring-slate-100" />
+                  <img
+                    src="/users/user-1.png"
+                    alt="user image"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-2.png"
+                    alt="user image"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-3.png"
+                    alt="user image"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-5.jpg"
+                    alt="user image"
+                    className="inline-block h-10 w-10 object-cover rounded-full ring-2 ring-slate-100"
+                  />
                 </div>
                 <div className="flex flex-col justify-between items-center sm:items-center">
                   <div className="flex gap-0.5">
-                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
                   </div>
-                  <p> <span className="font-semibold">1.250 </span>
-                   happy customers</p>
+                  <p>
+                    {" "}
+                    <span className="font-semibold">1.250 </span>
+                    happy customers
+                  </p>
                 </div>
               </div>
             </div>
           </div>
 
-
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
             <div className="realtive md:max-w-xl">
-              <img src="/your-image.png" className="absolute w-40 lg:2-52 left-56 select-none hidden sm:block lg:hidden xl:block" alt="" />
-              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" alt="" />
-              <Phone/>
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:2-52 left-56 select-none hidden sm:block lg:hidden xl:block"
+                alt=""
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt=""
+              />
+              <Phone className="w-64" imgSrc="/testimonial/1.jpg" dark />
             </div>
           </div>
-
         </MaxWidthWrapper>
+      </section>
+
+      {/*value proposition section */}
+      <section className="bg-slate-100py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-v=bold text-5xl md:text-6xl text-gray-900 ">
+              What our{" "}
+              <span className="relative px-2">
+                customers
+                <Icons.underline className="hidden sm:block pointer-events-none inset-x-0 -bottom-6 text-green-500 " />{" "}
+              </span>{" "}
+              say
+            </h2>
+            <img
+              src="/snake-2.png"
+              className="w-24 order-0 lg:order-2"
+              alt="sanke-pic"
+            />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "Cobra Case provided an amazing custom phone cover!{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    The quality is top-notch
+                  </span>{" "}
+                  and the design process was a breeze. Highly recommend!"
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Rajeev</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 2-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I recently ordered a custom phone cover from Cobra Case and
+                  couldn't be happier with the result! The design process was
+                  straightforward, and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    I was able to personalize my cover exactly how I wanted.
+                  </span>{" "}
+                  The quality of the material is exceptional, providing both
+                  durability and a sleek look. My cover arrived on time, and the
+                  customer service team was incredibly helpful throughout the
+                  process.{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    I highly recommend Cobra Case to anyone looking for a unique
+                    and high-quality
+                  </span>{" "}
+                  phone cover!"
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="users/user-4.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Rakesh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 2-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+        <div className="pt-16">
+         <Reviews/>
+        </div>
       </section>
     </div>
   );
